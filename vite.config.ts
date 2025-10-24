78311baed3350358824ccb85c6cd0885f2e52d46
Joachim Dalen <15696189+joachimdalen@users.noreply.github.com>
2025-10-24 15:50:36 +0200

    feat: add configuration based on env

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:51515",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "KAU_");
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.KAU_KOPIA_ENDPOINT,
+          changeOrigin: true,
+          secure: false,
+          configure: (_, options) => {
+            options.auth = `${env.KAU_KOPIA_USERNAME}:${env.KAU_KOPIA_PASSWORD}`;
+          },
+        },
       },
     },
-  },
+  };
 });
