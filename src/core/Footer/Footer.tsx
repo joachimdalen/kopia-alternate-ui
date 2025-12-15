a684149420db3a431cfe18ea8b0418bc0282a910
Joachim Dalen <15696189+joachimdalen@users.noreply.github.com>
2025-12-15 15:28:13 +0100

    fix: remove other theme toggle

--- src/core/Footer/Footer.tsx
+++ src/core/Footer/Footer.tsx
@@ -9,7 +9,6 @@ import useApiRequest from "../hooks/useApiRequest";
 import { useInterval } from "../hooks/useInterval";
 import IconWrapper from "../IconWrapper";
 import kopiaService from "../kopiaService";
-import { ThemeToggle } from "../ThemeToggle/ThemeToggle";
 import type { TasksSummary } from "../types";
 
 export function Footer() {
@@ -35,15 +34,12 @@ export function Footer() {
     <AppShellFooter p="xs">
       <Group justify="space-between">
         <Group>
-          <ThemeToggle />
-          <Group>
-            <Anchor href="" target="_blank" fz="sm">
-              <Group gap={5}>
-                <IconWrapper icon={IconBrandGithub} size={16} />
-                <Text fz="sm">Kopia Alternate UI</Text>
-              </Group>
-            </Anchor>
-          </Group>
+          <Anchor href="" target="_blank" fz="sm">
+            <Group gap={5}>
+              <IconWrapper icon={IconBrandGithub} size={16} />
+              <Text fz="sm">Kopia Alternate UI</Text>
+            </Group>
+          </Anchor>
         </Group>
         <Group>
           <Tooltip label={`${data.SUCCESS} task(s) completed`}>
