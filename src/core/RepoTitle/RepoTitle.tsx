6cff5a86ce24be2c6257e4349749d0d8ae971bb5
Joachim Dalen <15696189+joachimdalen@users.noreply.github.com>
2026-01-04 07:46:31 +0100

    refactor: remove unused repo title component

--- src/core/RepoTitle/RepoTitle.tsx
+++
@@ -1,7 +0,0 @@
-import { Title } from "@mantine/core";
-import { useAppContext } from "../context/AppContext";
-
-export default function RepoTitle() {
-  const { repoStatus } = useAppContext();
-  return <Title order={1}>{repoStatus.description}</Title>;
-}
