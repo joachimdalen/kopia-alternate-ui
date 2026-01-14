a9b45c46d6c52cb63b7ee375137fa0bc36bd6800
Joachim Dalen <15696189+joachimdalen@users.noreply.github.com>
2026-01-14 20:18:35 +0100

    refactor(core): group links in header (#99)
    ## Changes
    
    Grouped some of the links in the header to allow better navigation of
    planned features.
    
    <img width="719" height="226" alt="image"
    src="https://github.com/user-attachments/assets/97c7d8da-abb8-4960-a6ba-3143860d2696"
    />
    
    
    ### Checklist
    
    - [x]  Files have been formatted - `npm run format:fix`
    - [x] Languge files have been updated  - `npm run lang:extract`
    - [x] Tests passes - `npm run test`

--- src/core/Header/Header.tsx
+++ src/core/Header/Header.tsx
@@ -1,8 +1,23 @@
 import { t } from "@lingui/core/macro";
-import { Anchor, AppShellHeader, Box, Burger, Container, Group, Text } from "@mantine/core";
+import {
+  Anchor,
+  AppShellHeader,
+  Box,
+  Burger,
+  Center,
+  Container,
+  Divider,
+  Group,
+  HoverCard,
+  SimpleGrid,
+  Text,
+  ThemeIcon,
+  UnstyledButton
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import {
   IconBrandGithub,
+  IconChevronDown,
   IconClipboardCheck,
   IconDatabase,
   IconFileCertificate,
@@ -10,70 +25,198 @@ import {
   IconPackage,
   IconSettings
 } from "@tabler/icons-react";
+import cx from "clsx";
 import { useMemo } from "react";
-import { Link, NavLink } from "react-router";
+import { Link, NavLink, useLocation } from "react-router";
 import { useAppContext } from "../context/AppContext";
 import IconWrapper from "../IconWrapper";
 import classes from "./Header.module.css";
+
+type Link1 = {
+  id: string;
+  label: string;
+  disabled?: boolean;
+  links: Link2[];
+  icon: typeof IconSettings;
+};
+type Link2 = {
+  link: string;
+  label: string;
+  disabled?: boolean;
+  icon: typeof IconSettings;
+  description?: string;
+};
+type AllLinks = Link1 | Link2;
+
 export function Header() {
   const [opened, { toggle }] = useDisclosure(false);
   const { repoStatus } = useAppContext();
+  const location = useLocation();
 
-  const links = useMemo(() => {
+  const links: AllLinks[] = useMemo(() => {
     return [
       {
-        link: "/snapshots",
+        id: "snapshots-section",
         label: t`Snapshots`,
         disabled: !repoStatus.connected,
-        icon: IconPackage
+        icon: IconPackage,
+        links: [
+          {
+            link: "/snapshots",
+            label: t`Snapshots`,
+            disabled: !repoStatus.connected,
+            icon: IconPackage,
+            description: t`Manage snapshots`
+          },
+          {
+            link: "/mounts",
+            label: t`Mounts`,
+            disabled: !repoStatus.connected,
+            icon: IconFolderBolt,
+            description: t`Manage mounted snapshots`
+          }
+        ]
       },
       {
-        link: "/mounts",
-        label: t`Mounts`,
-        disabled: !repoStatus.connected,
-        icon: IconFolderBolt
-      },
-      {
-        link: "/policies",
-        label: t`Policies`,
-        disabled: !repoStatus.connected,
-        icon: IconFileCertificate
+        id: "repo-section",
+        label: t`Repository`,
+        icon: IconDatabase,
+        links: [
+          {
+            link: "/repo",
+            label: t`Status`,
+            icon: IconDatabase,
+            description: t`Manage repository status and configuration`
+          },
+          {
+            link: "/policies",
+            label: t`Policies`,
+            disabled: !repoStatus.connected,
+            icon: IconFileCertificate,
+            description: t`Manage snapshot policies for this repository`
+          }
+        ]
       },
       { link: "/tasks", label: t`Tasks`, icon: IconClipboardCheck },
-      { link: "/repo", label: t`Repository`, icon: IconDatabase },
       { link: "/preferences", label: t`Preferences`, icon: IconSettings },
       {
         link: "ext:https://github.com/joachimdalen/kopia-alternate-ui",
         label: "GitHub",
         icon: IconBrandGithub
       }
-    ];
+    ] satisfies AllLinks[];
   }, [repoStatus]);
 
-  const items = links.map((link) =>
-    link.disabled ? (
-      <a key={link.label} aria-disabled className={classes.link}>
-        <Group gap={5}>
-          <IconWrapper icon={link.icon} size={14} />
-          {link.label}
-        </Group>
-      </a>
-    ) : link.link.startsWith("ext:") ? (
-      <Anchor key={link.label} href={link.link.replace("ext:", "")} className={classes.link} target="_blank">
-        <Group gap={5}>
-          <IconWrapper icon={link.icon} size={14} />
-          {link.label}
-        </Group>
-      </Anchor>
-    ) : (
+  const items = links.map((link) => {
+    if ("links" in link) {
+      const links2 = link.links.map((item) => {
+        const body = (
+          <Group wrap="nowrap" align="flex-start">
+            <ThemeIcon size={34} variant="default" radius="md">
+              <IconWrapper icon={item.icon} size={22} />
+            </ThemeIcon>
+            <div>
+              <Text size="sm" fw={500}>
+                {item.label}
+              </Text>
+              <Text size="xs" c="dimmed">
+                {item.description}
+              </Text>
+            </div>
+          </Group>
+        );
+
+        if (item.disabled) {
+          return (
+            <a
+              key={link.label}
+              aria-disabled
+              className={cx(classes.subLink, { [classes.active]: item.link === location.pathname })}
+            >
+              {body}
+            </a>
+          );
+        }
+
+        return (
+          <UnstyledButton
+            className={cx(classes.subLink, { [classes.active]: item.link === location.pathname })}
+            key={item.link}
+            component={NavLink}
+            to={item.link}
+          >
+            {body}
+          </UnstyledButton>
+        );
+      });
+      return (
+        <HoverCard
+          width={600}
+          position="bottom"
+          radius="md"
+          shadow="md"
+          withinPortal
+          key={link.id}
+          disabled={link.links.every((x) => x.disabled)}
+        >
+          <HoverCard.Target>
+            <UnstyledButton
+              className={cx(classes.link, { [classes.active]: link.links.some((x) => x.link === location.pathname) })}
+            >
+              <Center>
+                <IconWrapper icon={link.icon} size={14} />
+                <Box component="span" mx={5}>
+                  {link.label}
+                </Box>
+                <IconWrapper icon={IconChevronDown} size={14} />
+              </Center>
+            </UnstyledButton>
+          </HoverCard.Target>
+
+          <HoverCard.Dropdown style={{ overflow: "hidden" }}>
+            <Text fw={500} px="md">
+              {link.label}
+            </Text>
+
+            <Divider my="sm" />
+
+            <SimpleGrid cols={2} spacing={2}>
+              {links2}
+            </SimpleGrid>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      );
+    }
+
+    if (link.disabled) {
+      return (
+        <a key={link.label} aria-disabled className={classes.link}>
+          <Group gap={5}>
+            <IconWrapper icon={link.icon} size={14} />
+            {link.label}
+          </Group>
+        </a>
+      );
+    }
+    if (link.link.startsWith("ext:")) {
+      return (
+        <Anchor key={link.label} href={link.link.replace("ext:", "")} className={classes.link} target="_blank">
+          <Group gap={5}>
+            <IconWrapper icon={link.icon} size={14} />
+            {link.label}
+          </Group>
+        </Anchor>
+      );
+    }
+    return (
       <NavLink key={link.label} to={link.link} className={classes.link}>
         <Group gap={5}>
           <IconWrapper icon={link.icon} size={14} />
           {link.label}
         </Group>
       </NavLink>
-    )
-  );
+    );
+  });
 
   return (
     <AppShellHeader className={classes.header}>
